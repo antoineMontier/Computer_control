a01b7c4108d7741582db798697d18c709a88ce42
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2022-12-03 12:38:31 +0100

    Improvement of dino obstacle detection

--- dino_player.py
+++ dino_player.py
@@ -1,7 +1,7 @@
 import  os, pyautogui, time, sys, pyperclip
 from PIL import Image, ImageGrab
 
-        
+
 
 def resetZoomWindow():
     pyautogui.keyDown('ctrl')
@@ -17,8 +17,17 @@ def resetZoomWindow():
         time.sleep(1)
         pyautogui.press('esc')#start game
 
-"""
+def obstacleInFrontOfDino(array_of_px, start_x, start_y, length):
+    i = start_x
+    while(i < start_x + length):
+        if(array_of_px[i, start_y] != (255, 255, 255)):
+            return True
+        i+=2
+    return False
 
+
+
+"""
 answer = pyautogui.confirm("ready to start ?\n")
 if (answer != "OK"):
     print("bye")
@@ -52,24 +61,28 @@ region_y = y_axis
 region_w = dino_width
 region_h = dino_height
 
-iterations = 0
- last_jump = iterations
+
 
 pyautogui.press('space')#start game
+
+iterations = 0
+last_jump = iterations
 playing = True
 while playing:
     #do things....
-    print("iterations = %d" % iterations)
+    # print("iterations = %d" % iterations)
     iterations += 1
     px = ImageGrab.grab().load()
-    mouse = pyautogui.position() 
-    print(mouse)
-    print(region_x + region_w*1.5, region_y + region_h*0.3    )
-    print(px[region_x + region_w*1.5, region_y + region_h*0.3 ])
-    if iterations - last_jump > 30 and  px[region_x + region_w*1.5, region_y + region_h*0.3] != (255, 255, 255):
+    
+    #mouse = pyautogui.position() 
+    #print(mouse)
+    #print(region_x + region_w*1.65 , region_y + region_h*(-1)     )
+
+    if obstacleInFrontOfDino(px, region_x + region_w*1.5, region_y, region_w*0.4): 
+        print("eeeeee")
         last_jump = iterations
         pyautogui.press('space') 
-    #cactus = pyautogui.locateOnScreen('cactus_stick.png', region=(region_x,region_y, region_w, region_h), confidence=0.9)
+    #cactus = pyautogui.locateOnScreen('cactus_stick.png', region=(region_x ,region_y, region_w, region_h), confidence=0.9)
     #if(cactus != None):
     #    pyautogui.press('space')
 
